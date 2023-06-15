a5eb7d03e7e5d1819c61befa5302323a2d8f49e5
SoFatou <sokhnafatoumarieme95@gmail.com>
2023-06-15 14:05:45 +0000

    fairrjjdro

--- api_fewnu_compta/models/entree.py
+++ api_fewnu_compta/models/entree.py
@@ -4,7 +4,7 @@ from .client import Customer
 
 class Entree(models.Model):
     montant_entree = models.FloatField(blank=True, null=False, default=0)
-    nom_entree = models.IntegerField(null=True, blank=True)
+    nom_entree = models.CharField(null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     createdBy = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     archived = models.BooleanField(default=False)
