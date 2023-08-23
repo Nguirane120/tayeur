4b285a3fbe1b98026da998eb2970473771a8c9af
“Nguirane120” <badaranguirane120@gmail.com>
2023-08-23 19:36:44 +0000

    maj

--- backoffice/urls.py
+++ backoffice/urls.py
@@ -6,9 +6,10 @@ urlpatterns = [
     path("", dashboard, name="dashboard"),
     path('users', usersList, name='users'),
     path('users/<int:id>', userDetail, name='user-detail'),
+    path('users/<int:id>/', deleteUser, name='delete-user'),
     path('clients', clientsList, name='clients'),
     path('commandes', commandeList, name='commandes'),
     path("login", loginPage, name="login"),
     path("logout", logOutUser, name="logout")
-    
+
 ]   
--- backoffice/views/users.py
+++ backoffice/views/users.py
@@ -1,14 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from api_fewnu_compta.models import User
+from ..decorators import admin_only, unauthenticated_user, allowed_user
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
+
+
+@login_required(login_url='login')
+@allowed_user(allowed_roles=['admin'])
+@admin_only
 def usersList(request):
     users = User.objects.all().order_by("-id")
     context = {"users":users}
     return render(request, "backoffice/users/userList.html", context)
 
-
+@login_required(login_url='login')
+@allowed_user(allowed_roles=['admin'])
 def userDetail(request, id):
     user = User.objects.get(id=id)
     context = {"user":user}
     return render(request, "backoffice/users/userDetail.html",  context)
+
+
+
+@login_required(login_url='login')
+@allowed_user(allowed_roles=['admin'])
+def deleteUser(request, id):
+        user = User.objects.get(id=id)
+        if request.method == "POST":
+            user.delete()
+            messages.success(request, "Utilisateur " + user.firstName + " supprime avec success ")
+            return redirect('users')
+        context = {"user":user}
+        return render(request, 'backoffice/users/deleteUser.html', context)
